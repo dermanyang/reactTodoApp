661a12e05bf7c7544dcdc0770fbb57e328df662a
dermanyang <seandermanyang@gmail.com>
2018-07-27 16:06:37 -0700

    add buttons

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>React says hello!</p>,
-    document.getElementById('root'));
+//temporary data
+var dummyData = [
+  "Wash the dishes",
+  "Do homework",
+  "Walk the dog",
+  "Do laundry"
+]
+class TodoList extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <ul>
+        {dummyData.map(item => <li><button>X</button>{' '}{item}</li>)}
+      </ul>
+    );
+  }
+}
+
+
+ReactDOM.render(<TodoList/>,
+  document.getElementById('root'));
